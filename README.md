ada3a3c65dd41fb7402dc3dc1e64b7a3aa9b4ef3
Pallagi Joe <pallagijoe@gmail.com>
2024-04-18 15:48:15 +0200

    Add initial project files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# random-post-react
+# random-post
+Extend the App component to display a random post from 'https://jsonplaceholder.typicode.com/' API. Each time the 'Get Random Post' button is clicked, a new post should be fetched from the API and rendered to the DOM.
+
+Example API URL to fetch a post
+https://jsonplaceholder.typicode.com/posts/1
+(Post id must be between 1 and 100)
+
+level: junior-3
+
+https://react.dev/learn/writing-markup-with-jsx
+https://react.dev/reference/react/useState
+https://react.dev/learn/responding-to-events
+https://react.dev/learn/updating-objects-in-state
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [currentPost, setCurrentPost] = useState({});
+
+  async function fetchRandomPost() {
+     // TODO: fetch a random post from https://jsonplaceholder.typicode.com/posts/${post_id}
+  }
+
+  return (
+    <>
+    {/* dispaly your post here */}
+      <div className="card">
+        <h2>{/* display the post title here */}</h2>
+        <p>{/* display the post body here */}</p>
+      </div>
+      <br />
+      <button onClick={() => fetchRandomPost()}>Get Random Post</button>
+    </>
+  );
+}
+
+export default App;
